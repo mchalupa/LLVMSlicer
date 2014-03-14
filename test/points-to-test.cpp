97f62d642967860c216be4ed0d27e2e88c37ce55
Marek Chalupa <mchqwerty@gmail.com>
2014-03-14 19:54:33 +0100

    tests: add points-to-test
    actually it's not a test, it just dump PointsToGraph that is created
    according some sequence of a->b assigments
    I'm happy it looks like it works :)

---
+++ test/points-to-test.cpp
@@ -0,0 +1,107 @@
+#include <llvm/LLVMContext.h>
+#include <llvm/Function.h>
+#include <llvm/Module.h>
+#include <llvm/Support/IRReader.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include "../src/PointsTo/PointsTo.h"
+
+using namespace llvm;
+
+typedef ptr::PointsToGraph::Pointer Pointer;
+typedef ptr::PointsToGraph::Pointee Pointee;
+
+static ptr::ProgramStructure *PS;
+
+static void addPointsTo(Module &M, ptr::PointsToGraph &PTG,
+                        const char *a, const char *b)
+{
+    static std::map<const char *, Pointer> valueMap;
+    std::map<const char *, Pointer>::iterator ai, bi;
+    Pointer pa, pb;
+
+    ai = valueMap.find(a);
+    bi = valueMap.find(b);
+
+    if (ai == valueMap.end()) {
+        Value *va = new GlobalVariable(M, IntegerType::get(M.getContext(), 32),
+                                         false,
+                                         GlobalValue::CommonLinkage, 0 , a);
+        pa = Pointer(va, -1);
+        // use default copy constructor
+        valueMap.insert(std::make_pair(a, pa));
+    } else {
+        pa = ai->second;
+    }
+
+    if (bi == valueMap.end()) {
+        Value *vb = new GlobalVariable(M, IntegerType::get(M.getContext(), 32),
+                                          false,
+                                          GlobalValue::CommonLinkage, 0 , b);
+        pb = Pointer(vb, -1);
+        valueMap.insert(std::make_pair(b, pb));
+    } else {
+        pb = bi->second;
+    }
+
+    PTG.insert(pa, pb);
+}
+
+static void buildPointsToGraph(Module &M, void (*seq)(Module&, ptr::PointsToGraph&))
+{
+    ptr::PointsToCategories *categ = new ptr::AllInOneCategory();
+    ptr::PointsToGraph PTG(*PS, categ);
+
+    seq(M, PTG);
+
+    PTG.dump();
+}
+
+void figure1(Module& M, ptr::PointsToGraph& PTG)
+{
+    addPointsTo(M, PTG, "a", "b");
+    addPointsTo(M, PTG, "b", "c");
+    addPointsTo(M, PTG, "a", "d");
+    addPointsTo(M, PTG, "d", "e");
+}
+
+void figure2(Module& M, ptr::PointsToGraph& PTG)
+{
+    addPointsTo(M, PTG, "x", "v1");
+    addPointsTo(M, PTG, "x", "v2");
+    addPointsTo(M, PTG, "y", "v1");
+}
+
+void figure3(Module& M, ptr::PointsToGraph& PTG)
+{
+    addPointsTo(M, PTG, "a", "b");
+    addPointsTo(M, PTG, "a", "c");
+    addPointsTo(M, PTG, "a", "d");
+    addPointsTo(M, PTG, "c", "d");
+}
+
+
+
+int main(int argc, char **argv)
+{
+	LLVMContext context;
+	SMDiagnostic SMD;
+	Module M("points-to-test", getGlobalContext());
+
+    ptr::ProgramStructure ps(M);
+    PS = &ps;
+
+    errs() << "--== figure1 ==-- \n";
+    errs() << "===============================================\n";
+    buildPointsToGraph(M, figure1);
+
+    errs() << "--== figure2 ==-- \n";
+    errs() << "===============================================\n";
+    buildPointsToGraph(M, figure2);
+
+    errs() << "--== figure3 ==-- \n";
+    errs() << "===============================================\n";
+    buildPointsToGraph(M, figure3);
+
+	return 0;
+}
