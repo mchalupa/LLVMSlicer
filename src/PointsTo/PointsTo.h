a2bc23f487f63d9620274cd81622baafbcd0cd9d
Marek Chalupa <mchqwerty@gmail.com>
2014-03-16 08:29:20 +0100

    PointsToGraph: use const ProgramStructure
    We don't change program structure..

--- src/PointsTo/PointsTo.h
+++ src/PointsTo/PointsTo.h
@@ -133,7 +133,7 @@ namespace ptr {
     {
     public:
         // will build the points-to graph right from the constructor
-        PointsToGraph(ProgramStructure *PS, PointsToCategories *PTC)
+        PointsToGraph(const ProgramStructure *PS, PointsToCategories *PTC)
         :PS(PS), PTC(PTC)
         {
             buildGraph();
@@ -215,7 +215,7 @@ namespace ptr {
         Node *findNode(Pointee p) const;
 
         std::set<Node *> Nodes;
-        ProgramStructure *PS;
+        const ProgramStructure *PS;
         PointsToCategories *PTC;
     };
 
